8e08ac56ad01e056171f4633d67b7dbe2ea60fbf
kalpana <kalpana@192.168.1.110>
2017-02-23 19:37:07 +0100

    add output

--- src/FileProcess.java
+++ src/FileProcess.java
@@ -19,6 +19,7 @@ public class FileProcess {
 		//do something with the inputString from here
 		//e.g. to use the inputString to construct the dto, etc...
 		HC2017DTO dto = new HC2017DTO(inputString);
+		
 	}
 	public List<String> readInputFile(String path) throws IOException {
 		File file = new File(path);
