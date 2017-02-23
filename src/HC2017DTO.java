28ddc45192bb03c23cf44b9f2c1e64434c9254a7
AdamJin <AdamJin@192.168.1.20>
2017-02-23 20:35:16 +0100

    dto

--- src/HC2017DTO.java
+++ src/HC2017DTO.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * this is the object to which the input file should be rendered
@@ -16,12 +18,47 @@ public class HC2017DTO {
 
 	//constructor which is initialized by the input string info
 	public HC2017DTO(List<String> strList){
-		String line0[] = strList.get(0).split(" ");
-		this.numV = Integer.parseInt(line0[0]);
+		int curser = 0; //the curser which points to the current line
+		String line0[] = strList.get(curser).split(" ");
+		this.numV = Integer.parseInt(line0[0]); 
 		this.numEP = Integer.parseInt(line0[1]);
 		this.numRequestD = Integer.parseInt(line0[2]);
 		this.numCachedServers = Integer.parseInt(line0[3]);
-		this.cachedServersCapacity = Integer.parseInt(line0[4]);;
+		this.cachedServersCapacity = Integer.parseInt(line0[4]);
+		curser++;
+		String line1[] = strList.get(curser).split(" ");
+		List<Video> videos = new ArrayList<>();
+		for(int i=0; i< line1.length; i++){
+			Video v = new Video();
+			v.setId(i);
+			v.setSize(Integer.parseInt(line1[i]));
+			videos.add(v);
+			System.out.println("Video " + v.getId() + " has size: " + v.getSize());
+		}
+
+		curser++;
+		List<Endpoint> endpopints = new ArrayList<>();
+		//the outer loop populates the End points
+		for(int i = 0 ; i < numEP; i++){
+			String [] a = strList.get(curser).split(" ");
+			
+			Endpoint endpoint = new Endpoint();
+			endpoint.setId(i);
+			endpoint.setDataCenterLatency(Integer.parseInt(a[0]));
+			endpoint.setNumberOfCaches(Integer.parseInt(a[1]));
+			
+			curser++;
+			//the inner loop populates the map in each end point
+			for(int j= 0; j<endpoint.getNumberOfCaches(); j++){
+				String [] b = strList.get(curser).split(" ");
+				Map<Integer, Integer> cacheEndpointLatencyMap = new HashMap<Integer, Integer>();
+				cacheEndpointLatencyMap.put(Integer.parseInt(b[0]), Integer.parseInt(b[1]));
+				endpoint.setCacheEndpointLatencyMap(cacheEndpointLatencyMap);
+				curser++;
+			}
+		}
+
+
 
 	}
 	
