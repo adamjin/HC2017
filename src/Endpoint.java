30614079c9df248e178432998754ea5aa5a99568
kalpana <kalpana@192.168.1.110>
2017-02-23 20:25:07 +0100

    Endpoint

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -3,15 +3,25 @@ import java.util.Map;
 public class Endpoint {
 	private int id;
 	private int dataCenterLatency;
-	private Map<Integer, Integer> cacheEndpointLatencyMap;
+	private int numberOfCaches;
+	private Map<Integer, Integer> cacheEndpointLatencyMap;//key is cache id, value is latency
 	
+	public int getNumberOfCaches() {
+		return numberOfCaches;
+	}
+
+	public void setNumberOfCaches(int numberOfCaches) {
+		this.numberOfCaches = numberOfCaches;
+	}
+
 	public Endpoint(){
 		
 	}
 	
-	public Endpoint(int id, int dataCenterLatency, Map<Integer, Integer> cacheEndpointLatencyMap) {
+	public Endpoint(int id, int dataCenterLatency,int numberOfCaches, Map<Integer, Integer> cacheEndpointLatencyMap) {
 		this.id = id;
 		this.dataCenterLatency = dataCenterLatency;
+		this.numberOfCaches = numberOfCaches;
 		this.cacheEndpointLatencyMap = cacheEndpointLatencyMap;
 	}
 	public int getId() {
