bf289448b69dd694a72de7da8f4d27a69459209d
kalpana <kalpana@192.168.1.110>
2017-02-23 20:21:37 +0100

    Endpoint

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -4,4 +4,33 @@ public class Endpoint {
 	private int id;
 	private int dataCenterLatency;
 	private Map<Integer, Integer> cacheEndpointLatencyMap;
+	
+	public Endpoint(){
+		
+	}
+	
+	public Endpoint(int id, int dataCenterLatency, Map<Integer, Integer> cacheEndpointLatencyMap) {
+		this.id = id;
+		this.dataCenterLatency = dataCenterLatency;
+		this.cacheEndpointLatencyMap = cacheEndpointLatencyMap;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getDataCenterLatency() {
+		return dataCenterLatency;
+	}
+	public void setDataCenterLatency(int dataCenterLatency) {
+		this.dataCenterLatency = dataCenterLatency;
+	}
+	public Map<Integer, Integer> getCacheEndpointLatencyMap() {
+		return cacheEndpointLatencyMap;
+	}
+	public void setCacheEndpointLatencyMap(Map<Integer, Integer> cacheEndpointLatencyMap) {
+		this.cacheEndpointLatencyMap = cacheEndpointLatencyMap;
+	}
+	
 }
